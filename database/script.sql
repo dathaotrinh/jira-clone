3cc5a2ca76e7a386c8eb3159d2d7fcbfa295e22d
Thao Trinh <admin@Thaos-MacBook-Pro.local>
2020-07-30 05:16:52 -0400

    create person table script

--- database/script.sql
+++ database/script.sql
@@ -6,13 +6,52 @@ use jira_clone;
 drop table if exists project;
 
 create table project (
-	  project_id int auto_increment,
+	  project_id varchar(100),
     project_title varchar(100) not null,
     project_key varchar(100) not null,
     project_image varchar(100),
     created_date timestamp,
     primary key project(project_id)
 );
+
+
+drop table if exists person;
+
+create table person (
+		person_id varchar(100) not null,
+		first_name varchar(100) not null,
+		last_name varchar(100) not null,
+		image_url varchar(100),
+		primary key person(person_id)
+);
+
+drop table if exists project_person;
+
+create table project_person (
+		person_id varchar(100)  not null,
+		project_id varchar(100)  not null,
+		role bit not null,  -- 0 is assignee 1 is lead
+        primary key project_person(person_id, project_id),
+        constraint fk_person_project foreign key (person_id)
+        references person(person_id),
+        constraint fk_project_person foreign key (project_id)
+        references project(project_id)
+);
+
+drop table if exists auth;
+
+create table auth (
+	auth_id varchar(100) not null,
+	username varchar(100) not null,
+	password varchar(100) not null,
+	email varchar(100) not null,
+	fk_person_id varchar(100) not null,
+	primary key auth(auth_id),
+	constraint fk_person foreign key (fk_person_id)
+	references person(person_id)
+);
+
+
 
 drop table if exists task;
 
@@ -29,11 +68,11 @@ create table task (
 drop table if exists project_task;
 
 create table project_task (
-	  fk_project_id int not null,
-    fk_task_id int not null,
-    primary key project_task(fk_project_id, fk_task_id),
-    constraint fk_project foreign key (fk_project_id)
+	project_id varchar(100) not null,
+    task_id int not null,
+    primary key project_task(project_id, task_id),
+    constraint fk_project_task foreign key (project_id)
     references project(project_id),
-    constraint fk_task foreign key (fk_task_id)
+    constraint fk_task_project foreign key (task_id)
     references task(task_id)
 );
