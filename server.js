dfb2ec6f01ceaa0dbb4138ac9f31f3ee8c1e4cd1
Thao Trinh <admin@Thaos-MacBook-Pro.local>
2020-07-30 15:20:54 -0400

    fix deployment

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const path = require('path');
+const app = express(); app.use(express.static(__dirname + '/jira-clone/myapp')); app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+ '/dist/jira-clone/index.html'));});
+app.listen(process.env.PORT || 8080);
