8ed60f87d8797f491351ffb8858bb83e5eac2915
Thao Trinh <admin@Thaos-MacBook-Pro.local>
2020-07-31 13:32:09 -0400

    fix directory

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const path = require('path');
-const app = express(); app.use(express.static(__dirname + '/dist/jira-clone')); app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+ '/dist/jira-clone/index.html'));});
+const app = express(); app.use(express.static(__dirname + '/dist/JiraClone')); app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+ '/dist/JiraClone/index.html'));});
 app.listen(process.env.PORT || 8080);
