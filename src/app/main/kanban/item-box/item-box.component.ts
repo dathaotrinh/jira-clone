da7e8b92d3869890e0803cc8dfbb07b6af2899cd
Thao Trinh <admin@Thaos-MacBook-Pro.local>
2020-08-07 03:05:43 -0400

    popup modal in item box

--- src/app/main/kanban/item-box/item-box.component.ts
+++ src/app/main/kanban/item-box/item-box.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { MainService } from 'src/app/shared/main.service';
 
 @Component({
   selector: 'app-item-box',
@@ -8,11 +9,111 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ItemBoxComponent implements OnInit {
 
   @Input() list = [];
-  constructor() { }
+  onDescription = false;
+
+  title = '';
+  description = '';
+  type = '';
+  reporterName = '';
+  priority = '';
+  avatar = '';
+  isVisible = false;
+  selectedStatus = null;
+
+  user = {
+    author: "",
+    avatar: ""
+  };
+
+  editorStyle = {
+    height: '100px'
+  };
+
+  constructor(private mainS: MainService) { }
 
   ngOnInit(): void {
   }
 
+  showModal(i: number, array: string[]): void {
+    this.mainS.getIssues().subscribe((data) => {
 
+      let temp = data.filter((ele) => ele.title === array[i]);
+
+      this.type = temp[0].issueType.toUpperCase() + '-' + temp[0].id;
+
+      this.title = temp[0].title;
+
+      this.description = temp[0].description;
+
+      this.isVisible = true;
+
+      this.selectedStatus = temp[0].issueStatus;
+
+      this.priority =
+        temp[0].issuePriority.charAt(0).toUpperCase() +
+        temp[0].issuePriority.substring(1, temp[0].issuePriority.length);
+
+      this.mainS.getUser(temp[0].reporterid).subscribe((user) => {
+        this.reporterName = user.name;
+        this.avatar = user.avatarUrl;
+
+        // this.user = {
+        //   author: this.reporterName,
+        //   avatar: this.avatar
+        // };
+      });
+    });
+  }
+
+
+  handleOk(): void {
+    this.isVisible = false;
+  }
+
+  handleCancel(): void {
+    this.isVisible = false;
+    this.onDescription = false;
+  }
+
+  priorityColor(priorityType: string): string {
+    if (priorityType === 'Medium') return 'orange';
+    else if (priorityType === 'Highest') return 'red';
+    else if (priorityType === 'High') return '#F06666';
+    else if (priorityType === 'Low') return 'green';
+    else if (priorityType === 'Lowest') return 'lightgreen';
+  }
+
+  onDescriptionChanged() {
+    this.onDescription = !this.onDescription;
+  }
+
+  data: any[] = [];
+  submitting = false;
+
+  inputValue = '';
+
+
+  handleSubmit(): void {
+    this.submitting = true;
+    const content = this.inputValue;
+    this.inputValue = '';
+    setTimeout(() => {
+      this.submitting = false;
+      this.data = [
+        ...this.data,
+        {
+          ...this.user,
+          content,
+          datetime: new Date(),
+          displayTime: new Date()
+        }
+      ].map(e => {
+        return {
+          ...e,
+          displayTime: e.datetime
+        };
+      });
+    }, 200);
+  }
 
 }
